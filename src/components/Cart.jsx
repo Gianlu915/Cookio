47d843c06c1fa46f3a2c88213c62fa2358410072
Gianluca Liccardo <gianluca_liccardo@libero.it>
2025-06-18 10:18:21 -0400

    Add UserProgressContext for cart and checkout progress management

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,16 +1,22 @@
-import { Modal } from 'bootstrap'
+import Modal from '../components/UI/Modal'
 import React, { useContext } from 'react'
 import CartContext from '../store/CartContext'
 import { currencyFormatter } from '../Util/formatting'
 import Button from './UI/Button'
+import  {UserProgressContext } from '../store/UserProgressContext'
 
 const Cart = () => {
     const cartCtx = useContext(CartContext);
+     const userProgressCtx = useContext(UserProgressContext);
 
     const cartTotal = cartCtx.items.reduce((totalPrice, item) => totalPrice + item.quantity * item.price,0);
 
+    function handleCloseCart() {
+        userProgressCtx.hideCart();
+    }
+
   return (
-    <Modal classname='cart'>
+    <Modal classname='cart' open={userProgressCtx.progress === 'cart'}>
         <h2>Your Cart</h2>
         <ul>
             {cartCtx.items.map(item => <li key={item.id}>
@@ -19,7 +25,7 @@ const Cart = () => {
         </ul>
         <p className='cart-total'>{currencyFormatter.format(cartTotal)}</p>
         <p className='modal-actions'>
-            <Button textOnly>Close</Button>
+            <Button textOnly onClick={handleCloseCart}>Close</Button>
             <Button>Go to checkout</Button>
         </p>
     </Modal>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,20 @@ import { useContext } from 'react'
 import logoImg from '../assets/logo.jpg'
 import Button from './UI/Button'
 import CartContext from '../store/CartContext'
+import { UserProgressContext } from '../store/UserProgressContext';
 
 export default function Header() {
 
     const cartCtx = useContext(CartContext);
+    const UserProgressCtx =  useContext(UserProgressContext)
 
      const totalCartItems = cartCtx.items.reduce((totalNumber, item) => {
     return totalNumber + item.quantity;
   }, 0);
+
+  function handleShowCart() {
+    UserProgressCtx.showCart();
+  }
 
     return <header id="main-header">
         <div id="title">
@@ -17,7 +23,7 @@ export default function Header() {
             <h1>Cookio</h1>
         </div>
         <nav>
-            <Button textOnly>Cart ({totalCartItems})</Button>
+            <Button textOnly onClick={handleShowCart}>Cart ({totalCartItems})</Button>
         </nav>
     </header>
 }
